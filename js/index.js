b42b70cd92a4986b50a062e775d839286547a7c1
Alejandro Daniel Di Stefano <addistefano76@gmail.com>
2022-10-14 22:00:13 -0300

    Aplicacion de Conceptos de la 1er Clase

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 // Importaciones
+const d = document;
+const menuBtn = d.querySelector('nav .btn');
+const menuList = d.querySelector('nav .menu');
 
+menuBtn.addEventListener('click', (e) => {
+    menuList.classList.toggle('active') ?
+    e.target.classList.replace('menu-open', 'menu-close') :
+    e.target.classList.replace('menu-close', 'menu-open') ;
 
+})
 
 
 
