281c7806d593f868f4cbe384f3028474c668883a
Alejandro Daniel Di Stefano <addistefano76@gmail.com>
2022-10-21 22:28:38 -0300

    Clase 2

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@
 const d = document;
 const menuBtn = d.querySelector('nav .btn');
 const menuList = d.querySelector('nav .menu');
+const prev = d.querySelector('.control.icon-prev');
+const next = d.querySelector('.control.icon-next');
+const gallery = d.querySelectorAll('.slide li');
+
 
 menuBtn.addEventListener('click', (e) => {
     menuList.classList.toggle('active') ?
@@ -10,6 +14,23 @@ menuBtn.addEventListener('click', (e) => {
 
 })
 
+prev.addEventListener('click', () => getItem('prev'))
+next.addEventListener('click', () => getItem('next'))
 
+function getItem(type) {
+    let nextElement;
+    for(let img of gallery){
+        if(img.classList.contains('active')){
+            img.classList.remove('active');
+            if (type == 'next'){
+                nextElement = img.nextElementSibling || img.parentNode.firstElementChild
+            }
+            else if (type == 'prev'){
+                nextElement = img.previousElementSibling || img.parentNode.lastElementChild
+            }
+        }
+    }
+    nextElement.classList.add('active')
+}
 
 // Autor: Alejandro Daniel Di Stefano
